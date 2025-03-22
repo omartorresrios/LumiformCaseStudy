3b17246f8f20995bee24f984d1edd87e60a4d4d5
Omar Torres <omar.torres@fueled.com>
2025-03-22 18:12:31 +0100

    Initial setup.

---
+++ LumiformCaseStudy/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  LumiformCaseStudy
+//
+//  Created by Omar Torres on 3/22/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		
+	}
+}
+
