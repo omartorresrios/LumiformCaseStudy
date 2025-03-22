bd71779e4c91e3398f8575b563c6487ef2f98cf4
Omar Torres <omar.torres@fueled.com>
2025-03-22 18:51:18 +0100

    feat: Data models
    - Add protocol-based models with polymorphism
    - Add a type erasure (GenericItem) to handle different type of items
    - Add testdata json file to test JSON parsing

---
+++ LumiformCaseStudy/Models.swift
@@ -0,0 +1,69 @@
+//
+//  Models.swift
+//  LumiformCaseStudy
+//
+//  Created by Omar Torres on 3/22/25.
+//
+
+protocol Item: Decodable {
+	var type: String { get }
+}
+
+class Page: Item {
+	let type: String
+	let title: String
+	let items: [GenericItem]
+}
+
+class Section: Item {
+	let type: String
+	let title: String
+	let items: [GenericItem]
+}
+
+protocol Question: Item {
+	var title: String { get }
+}
+
+class TextQuestion: Question {
+	let type: String
+	let title: String
+}
+
+class ImageQuestion: Question {
+	let type: String
+	let title: String
+	let src: String
+}
+
+struct GenericItem: Decodable {
+	private let _item: Item
+	
+	var asPage: Page? { return _item as? Page }
+	var asSection: Section? { return _item as? Section }
+	var asTextQuestion: TextQuestion? { return _item as? TextQuestion }
+	var asImageQuestion: ImageQuestion? { return _item as? ImageQuestion }
+	
+	init(from decoder: Decoder) throws {
+		let container = try decoder.container(keyedBy: CodingKeys.self)
+		let type = try container.decode(String.self, forKey: .type)
+		
+		switch type {
+		case "page":
+			self._item = try Page(from: decoder)
+		case "section":
+			self._item = try Section(from: decoder)
+		case "text":
+			self._item = try TextQuestion(from: decoder)
+		case "image":
+			self._item = try ImageQuestion(from: decoder)
+		default:
+			throw DecodingError.dataCorruptedError(forKey: .type, in: container,
+												   debugDescription: "Unknown type \(type)")
+		}
+	}
+	
+	private enum CodingKeys: String, CodingKey {
+		case type
+	}
+}
