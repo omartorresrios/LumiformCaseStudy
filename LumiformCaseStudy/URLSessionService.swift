4907dd42a7598f69baf14f1fb29c1b318ba51863
Omar Torres <omar.torres@fueled.com>
2025-03-23 19:33:36 +0100

    feat: Add GenericItemMapper to decode API response into GenericItem.
    - Implemented status code validation before attempting to decode.
    - Handled decoding errors by returning NetworkError cases.

--- LumiformCaseStudy/URLSessionService.swift
+++ LumiformCaseStudy/URLSessionService.swift
@@ -45,3 +45,19 @@ enum NetworkError: Error {
 	case connectivity
 	case invalidResponse(statusCode: Int)
 }
+
+final class GenericItemMapper {
+	
+	static func map(_ data: Data, _ response: HTTPURLResponse) -> FetchDataResult {
+		guard response.statusCode == 200 else {
+			return .failure(NetworkError.invalidResponse(statusCode: response.statusCode))
+		}
+		
+		do {
+		   let genericItem = try JSONDecoder().decode(GenericItem.self, from: data)
+		   return .success(genericItem)
+	   } catch {
+		   return .failure(NetworkError.invalidData)
+	   }
+	}
+}
