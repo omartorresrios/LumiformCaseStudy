21f2113411caeefe8c0cec864fe075b28b17bd1f
Omar Torres <omar.torres@fueled.com>
2025-03-23 19:22:28 +0100

    feat: Implement URLSessionService to perform network requests.
    - fetchData in URLSessionService only throws success or error.
    - Added default url in init for testing.

--- LumiformCaseStudy/URLSessionService.swift
+++ LumiformCaseStudy/URLSessionService.swift
@@ -15,3 +15,22 @@ enum ServiceResult {
 protocol ServiceProtocol {
 	func fetchData(completion: @escaping (ServiceResult) -> Void)
 }
+
+final class URLSessionService: ServiceProtocol {
+	private let urlString: String
+		
+	init(urlString: String = "https://run.mocky.io/v3/d403fba7-413f-40d8-bec2-afe6ef4e201e") {
+		self.urlString = urlString
+	}
+	
+	func fetchData(completion: @escaping (ServiceResult) -> Void) {
+		guard let url = URL(string: urlString) else { return }
+		URLSession.shared.dataTask(with: url) { data, response, error in
+			if let error = error {
+				completion(.failure(error))
+			} else if let data = data, let response = response as? HTTPURLResponse {
+				completion(.success(data, response))
+			}
+		}.resume()
+	}
+}
