0ebb1e3ba744fddb384893249f71bbafec2629a7
Omar Torres <omar.torres@fueled.com>
2025-03-24 18:33:57 +0100

    Merge feature/ui-prototype into main

---
+++ LumiformCaseStudy/Views/Cells/TextQuestionCell.swift
@@ -0,0 +1,56 @@
+//
+//  TextQuestionCell.swift
+//  LumiformCaseStudy
+//
+//  Created by Omar Torres on 3/24/25.
+//
+
+import UIKit
+
+final class TextQuestionCell: UITableViewCell {
+	private let titleLabel = UILabel()
+	private let containerView = UIView()
+	
+	override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+		super.init(style: style, reuseIdentifier: reuseIdentifier)
+		setupUI()
+	}
+	
+	required init?(coder: NSCoder) {
+		fatalError("init(coder:) has not been implemented")
+	}
+	
+	private func setupUI() {
+		selectionStyle = .none
+		
+		containerView.backgroundColor = UIColor(white: 0.98, alpha: 1.0)
+		containerView.layer.cornerRadius = 8
+		containerView.layer.borderWidth = 1
+		containerView.layer.borderColor = UIColor(white: 0.9, alpha: 1.0).cgColor
+		
+		titleLabel.numberOfLines = 0
+		titleLabel.font = UIFont.systemFont(ofSize: 16)
+		
+		contentView.addSubview(containerView)
+		containerView.addSubview(titleLabel)
+		
+		containerView.translatesAutoresizingMaskIntoConstraints = false
+		titleLabel.translatesAutoresizingMaskIntoConstraints = false
+		
+		NSLayoutConstraint.activate([
+			containerView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 6),
+			containerView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 16),
+			containerView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -16),
+			containerView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -6),
+			
+			titleLabel.topAnchor.constraint(equalTo: containerView.topAnchor, constant: 10),
+			titleLabel.leadingAnchor.constraint(equalTo: containerView.leadingAnchor, constant: 10),
+			titleLabel.trailingAnchor.constraint(equalTo: containerView.trailingAnchor, constant: -10),
+			titleLabel.bottomAnchor.constraint(equalTo: containerView.bottomAnchor, constant: -10)
+		])
+	}
+	
+	func configure() {
+		titleLabel.text = "Question title"
+	}
+}
---
+++ LumiformCaseStudy/Views/PageViewController.swift
@@ -0,0 +1,61 @@
+//
+//  PageViewController.swift
+//  LumiformCaseStudy
+//
+//  Created by Omar Torres on 3/23/25.
+//
+
+import Foundation
+import UIKit
+
+final class PageViewController: UIViewController {
+	private let tableView = UITableView()
+	
+	init() {
+		super.init(nibName: nil, bundle: nil)
+	}
+	
+	required init?(coder: NSCoder) {
+		fatalError("init(coder:) has not been implemented")
+	}
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		setupUI()
+	}
+	
+	private func setupUI() {
+		title = "Main Page"
+		view.backgroundColor = .white
+		
+		tableView.delegate = self
+		tableView.dataSource = self
+		tableView.separatorStyle = .none
+		tableView.estimatedRowHeight = 100
+		tableView.rowHeight = UITableView.automaticDimension
+		
+		tableView.register(SectionCell.self, forCellReuseIdentifier: "SectionCell")
+		tableView.register(TextQuestionCell.self, forCellReuseIdentifier: "TextQuestionCell")
+		tableView.register(ImageQuestionCell.self, forCellReuseIdentifier: "ImageQuestionCell")
+		
+		view.addSubview(tableView)
+		tableView.translatesAutoresizingMaskIntoConstraints = false
+		NSLayoutConstraint.activate([
+			tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+			tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+			tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+			tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+		])
+	}
+}
+
+extension PageViewController: UITableViewDelegate, UITableViewDataSource {
+	func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		return 1
+	}
+	
+	func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+		let cell = tableView.dequeueReusableCell(withIdentifier: "SectionCell", for: indexPath) as! SectionCell
+		return cell
+	}
+}
