8c4535bf9d8bedf2a9a878d9f5115222450fb6fc
Omar Torres <omar.torres@fueled.com>
2025-03-27 19:02:43 +0100

    feat: Add states to image question view model, and show loading indicator while loading images.

--- LumiformCaseStudy/ViewModels/ImageQuestionViewModel.swift
+++ LumiformCaseStudy/ViewModels/ImageQuestionViewModel.swift
@@ -8,10 +8,28 @@
 import Foundation
 
 final class ImageQuestionViewModel: GenericItemViewModel {
+	enum LoadingState {
+		case idle
+		case loading
+		case loaded
+		case error(Error)
+	}
+	
 	private let imageService: ImageService
 	private let question: ImageQuestion
 	private let _nestingLevel: Int
 	var onImageLoaded: ((Data) -> Void)?
+	var onStateChanged: ((LoadingState) -> Void)?
+	
+	private(set) var loadingState: LoadingState = .idle {
+		didSet {
+			DispatchQueue.main.async { [weak self] in
+				guard let self = self else { return }
+				self.onStateChanged?(self.loadingState)
+			}
+		}
+	}
+	
 	var type: String { return "image" }
 	
 	var questionTitle: String {
@@ -29,20 +47,29 @@ final class ImageQuestionViewModel: GenericItemViewModel {
 	}
 	
 	func loadImage(fullSize: Bool) {
-		if let url = URL(string: question.src) {
-			imageService.fetchImage(from: url) { [weak self] result in
-				if case .success(let imageData) = result {
-					let processedData: Data
-					if fullSize {
-						processedData = imageData
-					} else {
-						processedData = ImageCache.resizedImageData(imageData,
-																	toSize: CGSize(width: 100, height: 100)) ?? imageData
-					}
-					DispatchQueue.main.async {
-						self?.onImageLoaded?(processedData)
-					}
+		guard let url = URL(string: question.src) else {
+			loadingState = .error(NSError(domain: "", code: -1, userInfo: [NSLocalizedDescriptionKey: "Invalid URL"]))
+			return
+		}
+		
+		loadingState = .loading
+		
+		imageService.fetchImage(from: url) { [weak self] result in
+			guard let self = self else { return }
+			switch result {
+			case .success(let imageData):
+				let processedData: Data
+				if fullSize {
+					processedData = imageData
+				} else {
+					processedData = ImageCache.resizedImageData(imageData,
+																toSize: CGSize(width: 100, height: 100)) ?? imageData
 				}
+				self.onImageLoaded?(processedData)
+				self.loadingState = .loaded
+				
+			case .failure(let error):
+				self.loadingState = .error(error)
 			}
 		}
 	}
